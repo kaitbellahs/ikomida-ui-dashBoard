a6445f2136a60db5d5525da3eb49e3e3e38ec5c8
khalid Ait Bellahs <kaitbellahs@gmail.com>
2022-05-19 14:31:19 -0300

    update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import App from './App.svelte';
 import { Network } from '@ikomida/components';
 
-Network.createInstance("http://192.168.1.104:1988", "com.ikomida.br.default", "vendor");
+Network.createInstance("https://api.ikomida.com", "com.ikomida.br.default", "vendor");
 
 const app = new App({
 	target: document.body
