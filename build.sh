ddeadbd22ef6914dbae38288c3a1b6a89da9f4c9
Khalid Ait Bellahs <kaitbellahs@gmail.com>
2022-10-16 16:18:00 -0300

    Release 1.0.0

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-# mv ./App/build/bundle.js ./App/build/bundle_old.js
-# yarn javascript-obfuscator  ./App/build/bundle_old.js --output  ./App/build/bundle.js --config JsObfuscator.js
-# rm ./App/build/bundle_old.js
+mv ./App/build/bundle.js ./App/build/bundle_old.js
+yarn javascript-obfuscator  ./App/build/bundle_old.js --output  ./App/build/bundle.js --config JsObfuscator.json
+rm ./App/build/bundle_old.js
 yarn cap copy
 yarn cap sync
